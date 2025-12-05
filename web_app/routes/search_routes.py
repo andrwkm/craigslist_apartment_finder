e61075a9f339ab8e711793413e9d357df1350de9
andrwkm <94572878+andrwkm@users.noreply.github.com>
2025-12-04 21:25:17 -0500

    Add search results route and template
    Introduces a new POST /search/results route to handle form submissions and display results. Updates the search form to use POST and initializes input values. Adds a basic search_results.html template.

--- web_app/routes/search_routes.py
+++ web_app/routes/search_routes.py
@@ -4,6 +4,16 @@ search_routes = Blueprint("search_routes", __name__)
 
 @search_routes.route("/search/form")
 @search_routes.route("/search")
-def form():
+def search_form():
     print("SEARCH FORM...")
     return render_template("search_form.html")
+
+
+@search_routes.route("/search/results", methods=["POST"])
+def search_results():
+    print("SEARCH RESULTS...")
+    request_data = dict(request.form)
+    print(f"Form Data Received: {request_data}")
+
+
+    return render_template("search_results.html")
